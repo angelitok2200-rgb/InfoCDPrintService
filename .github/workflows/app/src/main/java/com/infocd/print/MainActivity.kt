c7f7f6b434049baf9926ce5c7e9d5228d6cab857
angelitok2200-rgb <angelitok2200@gmail.com>
2026-08-13 00:25:09 -0500

    Create MainActivity.kt

---
+++ .github/workflows/app/src/main/java/com/infocd/print/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.infocd.print
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+
+    override fun onCreate(
+        savedInstanceState: Bundle?
+    ) {
+
+        super.onCreate(
+            savedInstanceState
+        )
+
+        val tv = TextView(this)
+
+        tv.text =
+            "InfoCD Print Service"
+
+        setContentView(tv)
+    }
+}
